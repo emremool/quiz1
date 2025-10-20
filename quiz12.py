aa46738c7bebd3025f7428fab9f6329d3b43b7b2
emremol <emremol3544@gmail.com>
2025-10-20 20:09:15 +0300

    quiz12

---
+++ quiz12.py
@@ -0,0 +1,5 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+full_name_upper = full_name.upper()
+print(f"Your full name in uppercase is: {full_name_upper}")
